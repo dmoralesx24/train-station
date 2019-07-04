b2c229ddf9e86346ad42a4d1e6f9b095d435d1d2
David Morales <dmoralesx24@gmail.com>
2019-07-03 23:38:52 -0400

    finished the html and bootstrap set up for webpage

---
+++ assets/javascript/train.js
@@ -0,0 +1,11 @@
+// firebase config
+
+var config = {
+    apiKey: "AIzaSyAd7nX4nwo7Y4lAkVggS5kSJjkGv2LiDHY",
+    authDomain: "train-homework-ad42d.firebaseapp.com",
+    databaseURL: "https://train-homework-ad42d.firebaseio.com",
+    projectId: "train-homework-ad42d",
+    storageBucket: "",
+    messagingSenderId: "182060293125",
+    appId: "1:182060293125:web:b87932c754951b6c"
+};
